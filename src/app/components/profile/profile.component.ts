fa258017924e60e830c5752f34468541b40cf4a0
kunikamakker <kmak2710@gmail.com>
2024-09-21 10:40:25 +0530

    panels

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -10,17 +10,57 @@ import { SharedModule } from '../../shared/shared.module';
 })
 export class ProfileComponent {
   profilePanels: any[] = [];
-  
+  visiblePanelName: string = 'userDetails';
   constructor() {
     this.profilePanels = [
       {
         text: 'User Details',
-        openFunc: () => {}
+        className: 'active',
+        openFunc: () => {
+          this.updatePanel('userDetails', 0); // Assuming this is the first panel
+        }
       },
       {
         text: 'Preferences',
-        openFunc: () => {}
+        className: '',
+        openFunc: () => {
+          this.updatePanel('preferences', 1); // Assuming this is the second panel
+        }
+      },
+      {
+        text: 'Order History',
+        className: '',
+        openFunc: () => {
+          this.updatePanel('orderHistory', 2); // Assuming this is the third panel
+        }
+      },
+      {
+        text: 'Help',
+        className: '',
+        openFunc: () => {
+          this.updatePanel('help', 3); // Assuming this is the fourth panel
+        }
+      },
+      {
+        text: 'Change Password',
+        className: '',
+        openFunc: () => {
+          this.updatePanel('changePassword', 4); // Assuming this is the fifth panel
+        }
+      },
+      {
+        text: 'Logout',
+        openFunc: () => {
+          console.log('logout');
+        }
       }
-    ]
+    ];    
+  }
+
+  updatePanel(panelName: string, index: number) {
+    this.visiblePanelName = panelName;
+    this.profilePanels.forEach((panel, i) => {
+      panel.className = i === index ? 'active' : ''; // Set 'active' for the selected panel
+    });
   }
 }
